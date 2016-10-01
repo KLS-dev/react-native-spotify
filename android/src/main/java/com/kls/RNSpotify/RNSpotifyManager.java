410ab3985fa106eafd2f0d313b97f1dce602537a
Cesar Landeros <chichozell@gmail.com>
2016-10-01 15:53:33 -0500

    added spotify sdk for android and bootstraped the setup method

--- android/src/main/java/com/kls/RNSpotify/RNSpotifyManager.java
+++ android/src/main/java/com/kls/RNSpotify/RNSpotifyManager.java
@@ -1,7 +1,11 @@
 package com.kls.RNSpotify;
 
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 
 public class RNSpotifyManager extends ReactContextBaseJavaModule {
 
@@ -11,7 +15,11 @@ public class RNSpotifyManager extends ReactContextBaseJavaModule {
 
     @Override
     public String getName() {
-        return "RNSpotifyManager";
+        return "RNSpotify";
     }
 
+    @ReactMethod
+    public void setup(String clientId, Promise promise) {
+
+    }
 }
